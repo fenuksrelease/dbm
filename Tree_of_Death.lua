3d3dd7f8e04b44a0db57f1dd41d4f211a26ddc8c
fenuksrelease <33070002+fenuksrelease@users.noreply.github.com>
2018-02-07 18:06:34 +0100

    Add files via upload

---
+++ Tree_of_Death.lua
@@ -0,0 +1,79 @@
+local mod	= DBM:NewMod("Tree_of_Death", "DBM-Ulduar")
+local L		= mod:GetLocalizedStrings()
+
+
+mod:SetRevision(("$Revision: 4133 $"):sub(12, -3))
+mod:SetCreatureID(570085)
+
+mod:RegisterCombat("combat")
+
+mod:RegisterEvents(
+	"SPELL_AURA_APPLIED",
+	"UNIT_SPELLCAST_START",
+	"SPELL_AURA_APPLIED_DOSE"
+)
+
+local WarnSoulShatter					= mod:NewAnnounce("SoulShatter", 2, 97328)
+local WarnSoulShatterSwitch			= mod:NewAnnounce("SoulShatterSwitch", 4, 97328)
+local WarnVoidBlast					= mod:NewTargetAnnounce(97330, 2)
+local WarnSummonAdds				= mod:NewAnnounce("SummonedAdds", 1)
+local SpecWarnDeathNova			= mod:NewSpecialWarning("specDeathNova")
+
+local timerSummonAdds				= mod:NewTimer(50, "NextSummonAdds")
+local timerDeathNova					= mod:NewTimer(80, "NextDeathNova")
+local timerVoidBlast					= mod:NewTimer(13, "NextVoidBlast")
+
+
+
+FirstAddWave = true
+function mod:OnCombatStart()	
+	timerSummonAdds:Start(32)
+	self:ScheduleMethod(32, "Adds")
+	timerDeathNova:Start()
+	timerVoidBlast:Start()
+	FirstAddWave = true
+end
+
+function mod:Adds()
+	if FirstAddWave == true then
+		WarnSummonAdds:Show()
+		timerSummonAdds:Start(53)
+		self:ScheduleMethod(53, "Adds")
+		FirstAddWave = false
+	else
+		WarnSummonAdds:Show()
+		timerSummonAdds:Start(50)
+		self:ScheduleMethod(50, "Adds")
+	end
+end
+
+function mod:SPELL_AURA_APPLIED(args)
+	if args:IsSpellID(97334) then -- Treant of Death
+		timerSummonAdds:Start()
+		WarnSummonAdds:Show()
+
+	elseif args:IsSpellID(97330) then -- Void Blast
+		WarnVoidBlast:Show(args.destName)
+		timerVoidBlast:Start()
+	end	
+end
+
+function mod:SPELL_AURA_APPLIED(args)
+	if args:IsSpellID(97328) then -- Find Soul Shatter
+		if args.amount == nil then args.amount = 1 end
+		if args.amount >= 1 and args.amount <= 2 then
+			WarnSoulShatter:Show(args.destName, args.amount)
+		elseif args.amount >= 3 then
+			WarnSoulShatterSwitch:Show(args.destName, args.amount)
+		end
+	end
+end
+
+function mod:UNIT_SPELLCAST_START(unit,spell)
+	if spell == "Death Nova" and unit == "boss1" then
+		SpecWarnDeathNova:Show()
+		timerDeathNova:Start()
+	end
+end
+
+mod.SPELL_AURA_APPLIED_DOSE = mod.SPELL_AURA_APPLIED
---
+++ Tree_of_Life.lua
@@ -0,0 +1,74 @@
+local mod	= DBM:NewMod("Tree_of_Life", "DBM-Ulduar")
+local L		= mod:GetLocalizedStrings()
+
+
+mod:SetRevision(("$Revision: 2248 $"):sub(12, -3))
+mod:SetCreatureID(570084)
+
+mod:RegisterCombat("combat")
+
+mod:RegisterEvents(
+	"SPELL_AURA_APPLIED",
+	"SPELL_SUMMON",
+	"SPELL_AURA_APPLIED_DOSE",
+	"UNIT_SPELLCAST_START"
+)
+
+local WarnBranchSwipe  				= mod:NewAnnounce("BranchSwipe", 2, 97312)
+local WarnBranchSwipeSwitch		= mod:NewAnnounce ("BranchSwipeSwitch", 4, 97312)
+local WarnSummonAdds				= mod:NewAnnounce("SummonedAdds", 1)
+local warnBarskin						= mod:NewAnnounce("warningBarskin", 2)
+
+local SpecWarnHealingTouch			= mod:NewSpecialWarning("SpecHealingTouch")
+
+
+
+local timerSummonAdds				= mod:NewTimer(50, "NextSummonAdds")
+local timerBarksin						= mod:NewTimer(80, "NextBarksin")
+local timerHealingTouch				= mod:NewTimer(85, "NextHealingTouch")
+
+AddsReset = false
+function mod:OnCombatStart()	
+	timerSummonAdds:Start(32)
+	timerBarksin:Start()
+	timerHealingTouch:Start()
+end
+
+function mod:Adds()
+	AddsReset = false
+end
+
+function mod:SPELL_SUMMON(args)
+	if args:IsSpellID(97318) then -- Adds
+		if AddsReset == false then 
+			WarnSummonAdds:Show()
+			timerSummonAdds:Start()
+			AddsReset = true 
+			self:ScheduleMethod(5, "Adds")
+		end
+	end
+end
+
+function mod:SPELL_AURA_APPLIED(args)
+	if args:IsSpellID(97310) then -- Barskin
+			warnBarskin:Show()
+			timerBarksin:Start()
+	
+	elseif args:IsSpellID(97312) then
+		if args.amount == nil then args.amount = 0 end
+		if args.amount >= 9 and args.amount <= 12 then
+			WarnBranchSwipe:Show(args.destName, args.amount)
+		elseif args.amount >= 13 then
+			WarnBranchSwipeSwitch:Show(args.destName, args.amount)
+		end
+	end
+end
+
+function mod:UNIT_SPELLCAST_START(unit,spell)
+	if spell == "Healing Touch"  and unit == "boss1" then
+		SpecWarnHealingTouch:Show()
+		timerHealingTouch:Start()
+	end
+end
+
+mod.SPELL_AURA_APPLIED_DOSE = mod.SPELL_AURA_APPLIED
